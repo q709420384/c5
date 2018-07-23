20fc63cf68270344b21b8ab7e479e2131f7abe19
q709420384 <709420384@qq.com>
2018-07-23 11:05:27 +0800

    all

---
+++ 18/springsnail/cmake-build-debug/CMakeFiles/springsnail.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sanyie/code/18/springsnail/conn.cpp" "/home/sanyie/code/18/springsnail/cmake-build-debug/CMakeFiles/springsnail.dir/conn.cpp.o"
+  "/home/sanyie/code/18/springsnail/fdwrapper.cpp" "/home/sanyie/code/18/springsnail/cmake-build-debug/CMakeFiles/springsnail.dir/fdwrapper.cpp.o"
+  "/home/sanyie/code/18/springsnail/log.cpp" "/home/sanyie/code/18/springsnail/cmake-build-debug/CMakeFiles/springsnail.dir/log.cpp.o"
+  "/home/sanyie/code/18/springsnail/main.cpp" "/home/sanyie/code/18/springsnail/cmake-build-debug/CMakeFiles/springsnail.dir/main.cpp.o"
+  "/home/sanyie/code/18/springsnail/mgr.cpp" "/home/sanyie/code/18/springsnail/cmake-build-debug/CMakeFiles/springsnail.dir/mgr.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 18/springsnail/cmake-build-debug/CMakeFiles/springsnail.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/springsnail.dir/conn.cpp.o"
+  "CMakeFiles/springsnail.dir/fdwrapper.cpp.o"
+  "CMakeFiles/springsnail.dir/log.cpp.o"
+  "CMakeFiles/springsnail.dir/main.cpp.o"
+  "CMakeFiles/springsnail.dir/mgr.cpp.o"
+  "springsnail.pdb"
+  "springsnail"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/springsnail.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
